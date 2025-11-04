a48ab28d9345dfaeaa1ebd3898ff3b48d931333f
Jayu3280 <patiljayant3280@gmail.com>
2025-11-04 16:13:14 +0530

    Add project files.

---
+++ myFirst/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myFirst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C#");
+            // Console.WriteLine("Enter Your Name");
+            //string name = Console.ReadLine(); // Jayant
+            //Console.WriteLine("Enter Your Addreess");
+            //string address = Console.ReadLine(); // Neri
+
+            //Console.WriteLine("My name is " + name + " My address is " + address); //Cocatination
+
+            //Console.WriteLine("My name is {0} and My address is {1}", name, address); //placeholder
+
+            //Console.WriteLine($"My name is {name} and My address is {address}"); //string interpolation
+
+            Console.Write("Jayant");
+            Console.Write("Patil");
+
+            Console.WriteLine("Enter the first Number");
+            int num1 = Convert.ToInt32(Console.ReadLine()); //int.Parse
+            Console.WriteLine("Enter the Second Number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int sum = num1 + num2;
+            Console.WriteLine($"Sum is {sum}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ myFirst/datatype.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myFirst
+{
+    internal class datatype
+    {
+        static void Main(string[] args)
+        {
+            //Value Types
+            int age = 25; //Integer Type
+            float height = 5.9f; //Floating type - 7 number
+            double weight = 0.12d; //15 to 16
+            decimal c = 1.898989898989m;//29
+            char grade = 'A';
+            bool isStudent = true;
+
+            //Reference Type
+            string name = "Jayant";
+
+            object anything = 11;
+            Console.WriteLine("name is " + name);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
+
+   
